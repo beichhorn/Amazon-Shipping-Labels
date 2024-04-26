7a3221803bbb5b448e762842ea9d1c585a5dc4c9
beichhorn <70973658+beichhorn@users.noreply.github.com>
2024-04-26 16:42:47 -0400

    Add files via upload

--- decodeLabels.py
+++ decodeLabels.py
@@ -1,35 +1,50 @@
-import sys, base64, codecs
+import sys, base64, os, codecs
  
 poNumber  = sys.argv[1]
 directory = sys.argv[2] 
 
+# Delete Files if already existing
+if os.path.exists(directory.strip() + "/" + poNumber.strip() + ".txt"):
+    os.remove(directory.strip() + "/" + poNumber.strip() + ".txt")
+
+if os.path.exists(directory.strip() + "/TRACKNO.txt"):
+    os.remove(directory.strip() + "/TRACKNO.txt")    
+
+base64_string = " "
+txt           = " "
+
 with open(directory + "/response.txt", "r") as g:
      str = g.read()
 
-# Extract Label
+# Extract Label(s)
 start = str.find('"content":') + 11
-end   = str.find('"}', start)
-txt   = str[start:end]
+end   = 0
+while start > 0 and start > end:
+    end   = str.find('"}', start) 
+    txt   = str[start:end]
+    # Label text is in Base64 format, need to decode
+    txt_bytes = txt.encode()
+    base64_bytes   = base64.b64decode(txt_bytes)
+    base64_string  = base64_bytes.decode(encoding='utf-8', errors='ignore')
+    
+    # Save Label(s)
+    with open(directory.strip() + "/" + poNumber.strip() + ".txt", "a") as f:
+        f.write(base64_string)
 
-# Label text is in Base64 format, need to decode
-txt_bytes = txt.encode()
-base64_bytes = base64.b64decode(txt_bytes)
-print(base64_bytes)
-base64_string = base64_bytes.decode(encoding='utf-8', errors='ignore')
+    # Keep searching 
+    start = str.find('"content":', end) + 11 
 
-# Save Label
-with open(directory.strip() + "/" + poNumber.strip() + ".txt", "w") as f:
-    f.write(base64_string)
-
-# Extract Tracking Number
+# Extract Tracking Number(s)
 start = str.find('"trackingNumber":') + 18
-end   = str.find('",', start);
-txt   = str[start:end]
+end   = 0
+txt   = " "
+while start > 0 and start > end:
+    end   = str.find('",', start);
+    txt   = str[start:end] + "\n"
 
-# Save Tracking Number
-with open(directory.strip() + "/TRACKNO.txt", "w") as f:
-    f.write(txt)
+    # Save Tracking Number(s)
+    with open(directory.strip() + "/TRACKNO.txt", "a") as f:
+      f.write(txt)
 
-
-        
-        
+    # Keep searching
+    start = str.find('"trackingNumber":', end) + 18
